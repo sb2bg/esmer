e1ecfe6b9c3cfe50a4e9abf5e4b5d38fdce37a6b
sb2bg <sullivanbognar@gmail.com>
2022-09-22 03:52:22 -0500

    Add basic lexer using logos

---
+++ src/lexer.rs
@@ -0,0 +1,29 @@
+use logos::Logos;
+
+#[derive(Logos, Debug, PartialEq)]
+pub enum Token {
+    #[token("+")]
+    Plus,
+
+    #[token("-")]
+    Minus,
+
+    #[token("*")]
+    Star,
+
+    #[token("/")]
+    Slash,
+
+    #[token("^")]
+    Caret,
+
+    #[regex(r"\d+(\.\d+)?", |lexer| lexer.slice().parse::<f64>())]
+    Number(f64),
+
+    #[regex("[a-zA-Z_][a-zA-Z0-9_]*", |lexer| lexer.slice().to_string())]
+    Identifier(String),
+
+    #[error]
+    #[regex(r"[ \t]+", logos::skip)] // skip whitespace
+    Error,
+}
